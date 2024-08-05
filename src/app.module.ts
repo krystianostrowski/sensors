af02c7c4cd0194262d4c1aa4d127a9367298d802
krystianostrowski <krystian.ostrowski@thefarm51.com>
2024-08-05 22:07:42 +0200

    Use sensors service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SensorsModule } from './sesnors/sensors.module';
+import { SensorGateway } from './sesnors/gateway/sensor.gateway';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/sensors'),
+    SensorsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SensorGateway],
 })
 export class AppModule {}
