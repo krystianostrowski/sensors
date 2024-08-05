cb88461f23f1d3c9cb96dcdfb4909d70e522412b
krystianostrowski <krystian.ostrowski@thefarm51.com>
2024-08-05 17:20:07 +0200

    Created sensors gateway

---
+++ src/sesnors/gateway/sensor.gateway.ts
@@ -0,0 +1,45 @@
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { SensorsService } from '../sensors.service';
+import { CreateSensorDto } from '../dto/create-sensor.dto';
+
+@WebSocketGateway()
+export class SensorGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  constructor(private readonly sensorsService: SensorsService) {}
+
+  afterInit(server: Server) {
+    console.log('WebSocket server init');
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Connected client ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected ${client.id}`);
+  }
+
+  @SubscribeMessage('sensorData')
+  async handleSensorData(@MessageBody() data: CreateSensorDto) {
+    const { temperature, humidity } = data;
+    if (typeof temperature === 'number' && typeof humidity === 'number') {
+      await this.sensorsService.create(data);
+      this.server.emit('sensorDataSaved', data);
+    } else {
+      this.server.emit('error', 'Invalid data format');
+    }
+  }
+}
