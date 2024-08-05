aacd992e7d14722029a11faeef353e90a4658682
krystianostrowski <krystian.ostrowski@thefarm51.com>
2024-08-05 17:13:44 +0200

    Created sensors service

---
+++ src/sesnors/sensors.service.ts
@@ -0,0 +1,19 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Sensor } from './interfaces/sensor.interface';
+import { CreateSensorDto } from './dto/create-sensor.dto';
+
+export class SensorsService {
+  constructor(
+    @InjectModel('Sensor') private readonly sensorModel: Model<Sensor>,
+  ) {}
+
+  async create(createSensorDto: CreateSensorDto): Promise<Sensor> {
+    const created = new this.sensorModel(createSensorDto);
+    return created.save();
+  }
+
+  async findAll(): Promise<Sensor[]> {
+    return this.sensorModel.find().exec();
+  }
+}
